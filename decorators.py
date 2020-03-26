ca13486b680feaae0f02064fd1f3d91b0133a8c9
jsneider05 <joan_sneider@hotmail.com>
2020-03-25 19:59:25 -0500

    Examples's decorators

---
+++ decorators.py
@@ -0,0 +1,35 @@
+PASSWORD = '12345'
+
+
+def password_required(func):
+  def wrapper():
+    password = input('Cual es tu contrasena?')
+
+    if password == PASSWORD:
+      return func()
+    else:
+      print('La contrasena no es correcta.')
+  return wrapper
+
+
+@password_required
+def needs_password():
+  print('La contraseña es correcta')
+
+
+def upper(func):
+  def wrapper(*args, **kwargs):
+    result = func(*args, **kwargs)
+
+    return result.upper()
+
+  return wrapper
+
+
+@upper
+def say_my_name(name):
+  return 'Hola, {}'.format(name)
+
+
+if __name__ == "__main__":
+    print(say_my_name('Joan'))
